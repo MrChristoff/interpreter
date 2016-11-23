0dea4dd3a5390655c36b68a57a1aa1c3b447c11e
Rosie <r.gollancz@gmail.com>
2016-11-23 12:42:03 +0000

    Added test setup

---
+++ src/Interpreter.hs
@@ -0,0 +1,29 @@
+module Interpreter where
+
+import Data.Char
+
+data Operator = Plus | Minus | Times | Div
+    deriving(Show, Eq)
+
+operator :: Char -> Operator
+operator c | c == '+' = Plus
+           | c == '-' = Minus
+           | c == '*' = Times
+           | c == '/' = Div
+
+data Token = TokenNumber Int
+           | TokenOperator Operator
+    deriving(Show)
+
+isSomeDigit :: Char -> Bool
+isSomeDigit c = elem c "0123456789"
+
+isSomeOp :: Char -> Bool
+isSomeOp c = elem c "+-*/"
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize (x : xs)
+   | isSomeDigit x = TokenNumber (digitToInt x) : tokenize xs
+   | isSomeOp x = TokenOperator (operator x) : tokenize xs
+   | otherwise = error ("Cannot tokenize " ++ [x])
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Interpreter
+
 main :: IO ()
 main = do
   putStrLn "hello world"
